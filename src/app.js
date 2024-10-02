86f8cfd27e6e312e8877ea7f1359e8b08874269d
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-10-02 23:52:27 +0530

    exploring route

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello from Santosh");
+
+// app.use("/user", (req, res) => {
+//     res.send("Haaaahaaa");
+// })
+app.get("/user", (req, res) => {
+    res.send({firstName: "Santosh", lastName: "Kumar"});
 })
-app.use("/hello", (req, res) => {
-    res.send("Hello my dear!"); 
+
+app.post("/user", (req, res) => {
+    res.send("Data successfully, saved to the database");
 })
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully");
+})
+
+//  this will match all the HTTP methods API calls to /test
 app.use("/test", (req, res) => {
     res.send("Hello from the server");
 })
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
