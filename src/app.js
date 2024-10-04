92bca3f167fe5eebf6fbd2d4e2ba8798dbd68812
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-10-05 03:20:26 +0530

    save the data in the database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+
+app.post("/signup", async (req, res) => {
+    // const userObj = {
+    //     firstName: "Nishant",
+    //     lastName: "Ranjan",
+    //     email: "nishant@gmail.com",
+    //     password: "tiger@123" 
+    // }
+
+    const user = new User({ 
+        firstName: "Nishant",
+        lastName: "Ranjan",
+        email: "nishant@gmail.com",
+        password: "123456"
+    });
+
+    // Creating a new instance of the User model
+    // const user = new User(userObj);
+    await user.save();
+    res.send("User Added successfully!");
+});
+
+
+
+
+
 
 
 connectDB()
