7f330f4ac971c2ebb0025794e9fa6de816ed59f0
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-13 07:08:26 +0530

    till Now

--- src/app.js
+++ src/app.js
@@ -3,43 +3,31 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-
 app.post("/signup", async (req, res) => {
-    // const userObj = {
-    //     firstName: "Nishant",
-    //     lastName: "Ranjan",
-    //     email: "nishant@gmail.com",
-    //     password: "tiger@123" 
-    // }
+  
 
-    const user = new User({ 
-        firstName: "Nishant",
-        lastName: "Ranjan",
-        email: "nishant@gmail.com",
-        password: "123456"
-    });
+  const user = new User({
+    firstName: "Nishant",
+    lastName: "Kumar",
+    email: "robin@gmail.com",
+    password: "robiN@123",
+  });
 
-    // Creating a new instance of the User model
-    // const user = new User(userObj);
+  try {
     await user.save();
     res.send("User Added successfully!");
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-
-
-
-
-
-
 connectDB()
-    .then(() => {
-        console.log("Connection is established!!");
-        app.listen(7777, () => {
-            console.log("Server is running on port 7777");
-        });
-    })
-    .catch((err) => {
-        console.error("Database can not be connected");
+  .then(() => {
+    console.log("Connection is established!!");
+    app.listen(7777, () => {
+      console.log("Server is running on port 7777");
     });
-
-
+  })
+  .catch((err) => {
+    console.error("Database can not be connected");
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connectDB = async () => {
     await mongoose.connect(
-        "",
+        process.env.MONGO_URI,
     );
 };
 
