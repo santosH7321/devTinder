7a8c4e5624df1ca96a520d09827aa3ab5a88bf85
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-13 07:51:17 +0530

    middleware for UserAuth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,33 @@
-export const adminAuth = (req, res, next) => {
-    console.log("Checking authorization");
-    const token = "xyz";
-    const isAuthenticated = token === "xyz";
-    if(!isAuthenticated){
-        res.status(401).send("Unauthorized");
-    } else {
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const UserAuth = async (req, res, next) => {
+    try {
+        const cookies = req.cookies;
+        const token = cookies;
+
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const decodedObject = jwt.verify(token, process.env.JWT_SECRET);
+        const { _id } = decodedObject;
+
+        const user = await User.findById({ _id });
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        req.user = user;
         next();
-    }
-};
-export const userAuth = (req, res, next) => {
-    console.log("Checking authorization");
-    const token = "xyz";
-    const isAuthenticated = token === "xyz";
-    if(!isAuthenticated){
-        res.status(401).send("Unauthorized");
-    } else {
-        next();
+
+    } catch (error) {
+        res.status(401).json({ message: "Unauthorized" });
+        
     }
 };
 
-
+export default UserAuth;
